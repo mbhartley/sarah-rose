a30742c608842fe0e3914d658481634f40f37f01
Matt Hartley <mbhartley1490@gmail.com>
2014-05-28 14:13:48 -0400

    spaces spaces spaces when scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,22 +10,9 @@ $(document).ready(function(){
 	    $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	        'scrollTop': $target.offset().top 
 	    }, 1000, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
 });
-
-// NAVBAR BACKGROUND COLOR APPEARS ON SCROLL DOWN
-
-window.addEventListener("scroll",function() { 
-   if(window.scrollY > 880) {
-   	console.log('HELLLLLLSOSO');
-      $('.navbar-fixed-top').addClass('active');
-      console.log("TWOOOO");
-   }
-   else {
-      $('.navbar').removeClass('active');
-   }
-},false);
